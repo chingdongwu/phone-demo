f405f3c9db1c139053a7bdb4683ce7014688b7f5
dd <remix@gmail.com>
2022-09-14 20:38:11 +0800

    基礎邏輯+爬蟲

--- js/index.js
+++ js/index.js
@@ -24,6 +24,19 @@ function analytics() {
         return;
     }
 
-    console.log(phoneNumber)
+    console.log(phoneNumber);
+    analyticsPhoneNumber(phoneNumber);
 }
-analytics()
+
+
+
+function analyticsPhoneNumber(phoneNumber) {
+    let fourNumber = phoneNumber.substring(phoneNumber.length - 4, phoneNumber.length)
+    console.log(fourNumber);
+    console.log(fourNumber / 80);
+    console.log(parseInt(fourNumber / 80));
+    let code = parseInt((fourNumber / 80 - parseInt(fourNumber / 80)) * 80);
+    console.log(code);
+    return code;
+}
+
